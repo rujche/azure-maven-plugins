a5fc55d61c6fe0a1691a847e8e2487a03319dbc5
Flanker32 <hanli@microsoft.com>
2024-04-28 09:36:36 +0800

    Update label for open streaming logs action

--- azure-toolkit-libs/azure-toolkit-containerregistry-lib/src/main/java/com/microsoft/azure/toolkit/lib/containerregistry/ContainerRegistry.java
+++ azure-toolkit-libs/azure-toolkit-containerregistry-lib/src/main/java/com/microsoft/azure/toolkit/lib/containerregistry/ContainerRegistry.java
@@ -169,7 +169,7 @@ public class ContainerRegistry extends AbstractAzResource<ContainerRegistry, Azu
         final Action<String> viewLogInBrowser = openUrl.bind(logSasUrl).withLabel("Open streaming logs in browser");
         final RegistryTaskRunStreamingLog urlStreamingLog = RegistryTaskRunStreamingLog.builder().logSasUrl(logSasUrl).task(run).build();
         final Action<StreamingLogSupport> viewLogInToolkit = AzureActionManager.getInstance().getAction(StreamingLogSupport.OPEN_STREAMING_LOG)
-            .bind(urlStreamingLog).withLabel("Open streaming logs in toolkit");
+            .bind(urlStreamingLog).withLabel("Open streaming logs");
         AzureMessager.getMessager().info(AzureString.format("Waiting for image building task run (%s) to be completed...", run.runId()), viewLogInToolkit, viewLogInBrowser);
         RunStatus status = run.status();
         while (waitingStatus.contains(status)) {
