8f5c59afa07e03c17705dc848709713d39ec658d
rujche <rujche@microsoft.com>
2024-08-26 09:38:26 +0800

    Add method: setArchetypeRelatedProperties.

--- azure-container-apps-maven-plugin/src/main/java/com/microsoft/azure/maven/container/apps/OrchestrateMojo.java
+++ azure-container-apps-maven-plugin/src/main/java/com/microsoft/azure/maven/container/apps/OrchestrateMojo.java
@@ -7,6 +7,7 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 
 import java.lang.reflect.Field;
+import java.util.Properties;
 
 @Mojo(name = "orchestrate")
 public class OrchestrateMojo extends CreateProjectFromArchetypeMojo {
@@ -14,6 +15,7 @@ public class OrchestrateMojo extends CreateProjectFromArchetypeMojo {
     public void execute() throws MojoExecutionException, MojoFailureException {
         System.out.println("Orchestrate Mojo started");
         updateSuperFields();
+        updateUserProperties();
         super.execute();
     }
 
@@ -22,25 +24,10 @@ public class OrchestrateMojo extends CreateProjectFromArchetypeMojo {
         setParentPrivateField("archetypeArtifactId", "azure-runtime-maven-tools");
         setParentPrivateField("archetypeVersion", "1.0-SNAPSHOT");
         setParentPrivateField("interactiveMode", false);
-        updateUserProperties();
     }
 
-    private void updateUserProperties() {
-        try {
-            Field field = CreateProjectFromArchetypeMojo.class.getDeclaredField("session");
-            field.setAccessible(true);
-            MavenSession session = (MavenSession) field.get(this);
-            session.getUserProperties().setProperty("groupId", "com.microsoft.azure.container.apps.maven.plugin.generated");
-            session.getUserProperties().setProperty("artifactId", "app-host");
-            session.getUserProperties().setProperty("version", "1.0.0-SNAPSHOT");
-            session.getUserProperties().setProperty("package", "jar");
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void setParentPrivateField(String fieldName, Object value) {
-        Field field = null;
+    private void setParentPrivateField(String fieldName, Object value) {
+        Field field;
         try {
             field = CreateProjectFromArchetypeMojo.class.getDeclaredField(fieldName);
             field.setAccessible(true);
@@ -48,6 +35,32 @@ public class OrchestrateMojo extends CreateProjectFromArchetypeMojo {
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void updateUserProperties() {
+        try {
+            Field field = CreateProjectFromArchetypeMojo.class.getDeclaredField("session");
+            field.setAccessible(true);
+            MavenSession session = (MavenSession) field.get(this);
+            Properties properties = session.getUserProperties();
+            setArtifactRelatedProperties(properties);
+            setArchetypeRelatedProperties(properties);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setArtifactRelatedProperties(Properties properties) {
+        properties.setProperty("groupId", "com.microsoft.azure.container.apps.maven.plugin.generated");
+        properties.setProperty("artifactId", "app-host");
+        properties.setProperty("version", "1.0.0-SNAPSHOT");
+        properties.setProperty("package", "jar");
+    }
+
+    private void setArchetypeRelatedProperties(Properties properties) {
+        properties.setProperty("moduleId", "com.microsoft.azure.container.apps.maven.plugin.generated.app.host");
+        properties.setProperty("includeAzure", "false");
+        properties.setProperty("includeSpring", "true");
     }
 
 }
